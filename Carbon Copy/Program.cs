8a6783eb08e0caff39151daa6b743e5dda6cc614
Jeremy Morton <github@game-point.net>
2008-11-01 23:52:17 +0000

    Enabled batch mode running.  Added to post-build to copy EXE and DLL to 'final' dir, ready for deployment (in Debug builds).

--- Carbon Copy/Program.cs
+++ Carbon Copy/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using Jez;
 
 namespace Carbon_Copy {
 	static class Program {
@@ -12,8 +13,36 @@ namespace Carbon_Copy {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			
-			// TODO: Need to implement commandline arguments here to allow for batch mode
-			Application.Run(new frmMain());
+			// If a file path has been specified, assume it's the path to an XML file
+			// containing settings for a backup profile, which we want to run in batch
+			// mode.
+			Jez.Utilities utils = new Jez.Utilities();
+			string[] cmdLineArgs = Environment.GetCommandLineArgs();
+			if (cmdLineArgs.Length > 2) {
+				utils.ShowError("Usage: CarbCopy.exe \"C:\\path\\to\\backup\\profile.xml\"");
+				Application.Exit();
+			}
+			else if (cmdLineArgs.Length == 2) {
+				// Run in batch mode - try to load options, then run straight from
+				// backup form
+				
+				// Load profile settings from selected XML file
+				CCOSaveLoad saveloader = new CCOSaveLoad();
+				CCO options;
+				string errorTxt;
+				string loadFromPath = cmdLineArgs[1];
+				if (!saveloader.Load(out options, loadFromPath, out errorTxt)) {
+					utils.ShowError("Error loading settings from '" + loadFromPath + "': " + errorTxt);
+					Application.Exit();
+				}
+				else {
+					Application.Run(new frmBackup(options));
+				}
+			}
+			else {
+				// Run in interactive mode
+				Application.Run(new frmMain());
+			}
 		}
 	}
 }
